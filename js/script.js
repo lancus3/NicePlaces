d889f935006591c1712043e23f3a6515c6d7e831
Szymon Lanc <lancus3@gmail.com>
2018-06-12 18:21:13 +0200

    Add restart button and progress bar

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
-  contain: true
+  contain: true,
+  hash: true
+  
 });
 
 // element argument can be a selector string
@@ -10,3 +12,16 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+var button = document.querySelector('.button');
+
+button.addEventListener( 'click', function( event ) {
+  flkty.selectCell(0);
+  });
+
+  var progressBar = document.querySelector('.progress-bar')
+
+  flkty.on( 'scroll', function( progress ) {
+    progress = Math.max( 0, Math.min( 1, progress ) );
+    progressBar.style.width = progress * 100 + '%';
+  });
