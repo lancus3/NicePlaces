ce8e977c5eab764e4487367e2136c4ff847e47c4
Szymon Lanc <lancus3@gmail.com>
2018-06-12 22:02:56 +0200

    Clean up

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ var listItems = '';
 var templateItem = document.getElementById('template-product-item').innerHTML;
 //Mustache.parse(templateItem);
 
-for(var i = 0; i < dataCarousel.length; i++){
-		//console.log(dataCarousel[i]);
-		listItems += Mustache.render(templateItem, dataCarousel[i]);
-  };
+for (var i = 0; i < dataCarousel.length; i++) {
+  //console.log(dataCarousel[i]);
+  listItems += Mustache.render(templateItem, dataCarousel[i]);
+};
 
 //var fullList = Mustache.render(listItems);
 
@@ -14,35 +14,24 @@ results.insertAdjacentHTML('beforeend', listItems);
 
 
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true,
   pageDots: false,
   hash: true
-  
+
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity('.main-carousel', {
   // options
 });
 
+var progressBar = document.querySelector('.progress-bar')
 
-
-
-
-
-var button = document.querySelector('.button');
-
-button.addEventListener( 'click', function( event ) {
-  flkty.selectCell(0);
-  });
-
-  var progressBar = document.querySelector('.progress-bar')
-
-  flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
-    progressBar.style.width = progress * 100 + '%';
-  });
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
+});
