9f1e8bc6403e7a7683fc8c7d590716e251822f65
Szymon Lanc <lancus3@gmail.com>
2018-06-12 21:39:37 +0200

    Add mustache template

--- js/script.js
+++ js/script.js
@@ -1,8 +1,24 @@
+
+var listItems = '';
+var templateItem = document.getElementById('template-product-item').innerHTML;
+//Mustache.parse(templateItem);
+
+for(var i = 0; i < dataCarousel.length; i++){
+		//console.log(dataCarousel[i]);
+		listItems += Mustache.render(templateItem, dataCarousel[i]);
+  };
+
+//var fullList = Mustache.render(listItems);
+
+results.insertAdjacentHTML('beforeend', listItems);
+
+
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
   contain: true,
+  pageDots: false,
   hash: true
   
 });
@@ -12,6 +28,11 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+
+
+
+
 
 var button = document.querySelector('.button');
 
