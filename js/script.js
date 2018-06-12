b10da9d972062cefd90db4b4790b8f1690bb38b9
Szymon Lanc <lancus3@gmail.com>
2018-06-12 22:07:12 +0200

    Clean up

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
 var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
+var flkty = new Flickity(elem, {
   // options
   cellAlign: 'left',
   contain: true,
   hash: true
-  
-});
 
+});
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity('.main-carousel', {
   // options
 });
 
 var button = document.querySelector('.button');
+var progressBar = document.querySelector('.progress-bar')
 
-
-
-  var progressBar = document.querySelector('.progress-bar')
-
-  flkty.on( 'scroll', function( progress ) {
-    progress = Math.max( 0, Math.min( 1, progress ) );
-    progressBar.style.width = progress * 100 + '%';
-  });
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
+});
